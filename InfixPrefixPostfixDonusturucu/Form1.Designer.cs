b0a2428928bd97f13987ae41de6ff4aaecad9d6c
bberkaybesli <bekirberkaybesli1@gmail.com>
2024-11-05 23:36:24 +0300

    Proje dosyası ekle.

---
+++ InfixPrefixPostfixDonusturucu/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace InfixPrefixPostfixConverter
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtInfix = new System.Windows.Forms.TextBox();
+            this.btnToPostfix = new System.Windows.Forms.Button();
+            this.btnToPrefix = new System.Windows.Forms.Button();
+            this.lblPostfix = new System.Windows.Forms.Label();
+            this.lblPrefix = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            this.txtInfix.Location = new System.Drawing.Point(12, 12);
+            this.txtInfix.Multiline = true;
+            this.txtInfix.Name = "txtInfix";
+            this.txtInfix.Size = new System.Drawing.Size(360, 60);
+            this.txtInfix.TabIndex = 0;
+ 
+            this.btnToPostfix.Location = new System.Drawing.Point(12, 78);
+            this.btnToPostfix.Name = "btnToPostfix";
+            this.btnToPostfix.Size = new System.Drawing.Size(175, 30);
+            this.btnToPostfix.TabIndex = 1;
+            this.btnToPostfix.Text = "Postfix\'e Dönüştür";
+            this.btnToPostfix.UseVisualStyleBackColor = true;
+            this.btnToPostfix.Click += new System.EventHandler(this.btnToPostfix_Click);
+
+            this.btnToPrefix.Location = new System.Drawing.Point(197, 78);
+            this.btnToPrefix.Name = "btnToPrefix";
+            this.btnToPrefix.Size = new System.Drawing.Size(175, 30);
+            this.btnToPrefix.TabIndex = 2;
+            this.btnToPrefix.Text = "Prefix\'e Dönüştür";
+            this.btnToPrefix.UseVisualStyleBackColor = true;
+            this.btnToPrefix.Click += new System.EventHandler(this.btnToPrefix_Click);
+
+            this.lblPostfix.AutoSize = true;
+            this.lblPostfix.Location = new System.Drawing.Point(12, 121);
+            this.lblPostfix.Name = "lblPostfix";
+            this.lblPostfix.Size = new System.Drawing.Size(52, 16);
+            this.lblPostfix.TabIndex = 3;
+            this.lblPostfix.Text = "Postfix: ";
+
+            this.lblPrefix.AutoSize = true;
+            this.lblPrefix.Location = new System.Drawing.Point(12, 150);
+            this.lblPrefix.Name = "lblPrefix";
+            this.lblPrefix.Size = new System.Drawing.Size(46, 16);
+            this.lblPrefix.TabIndex = 4;
+            this.lblPrefix.Text = "Prefix: ";
+
+            this.ClientSize = new System.Drawing.Size(384, 181);
+            this.Controls.Add(this.lblPrefix);
+            this.Controls.Add(this.lblPostfix);
+            this.Controls.Add(this.btnToPrefix);
+            this.Controls.Add(this.btnToPostfix);
+            this.Controls.Add(this.txtInfix);
+            this.Name = "Form1";
+            this.Text = "Infix, Prefix, Postfix Dönüştürücü";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox txtInfix;
+        private System.Windows.Forms.Button btnToPostfix;
+        private System.Windows.Forms.Button btnToPrefix;
+        private System.Windows.Forms.Label lblPostfix;
+        private System.Windows.Forms.Label lblPrefix;
+    }
+}
---
+++ InfixPrefixPostfixDonusturucu/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace InfixPrefixPostfixConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int OperatörÖnceliği(char op)
+        {
+            if (op == '+' || op == '-') return 1;
+            if (op == '*' || op == '/') return 2;
+            if (op == '^') return 3;
+            return 0;
+        }
+        private string InfixToPostfix(string infix)
+        {
+            var çıktı = new List<string>();
+            var yığın = new Stack<char>();
+
+            foreach (char karakter in infix)
+            {
+                if (char.IsLetterOrDigit(karakter))  
+                {
+                    çıktı.Add(karakter.ToString());
+                }
+                else if (karakter == '(')  
+                {
+                    yığın.Push(karakter);
+                }
+                else if (karakter == ')')  
+                {
+                    while (yığın.Count > 0 && yığın.Peek() != '(')
+                    {
+                        çıktı.Add(yığın.Pop().ToString());
+                    }
+                    yığın.Pop();  
+                }
+                else  
+                {
+                    while (yığın.Count > 0 && OperatörÖnceliği(yığın.Peek()) >= OperatörÖnceliği(karakter))
+                    {
+                        çıktı.Add(yığın.Pop().ToString());
+                    }
+                    yığın.Push(karakter);  
+                }
+            }
+
+            while (yığın.Count > 0)
+            {
+                çıktı.Add(yığın.Pop().ToString());
+            }
+
+            return string.Join(" ", çıktı); 
+        }
+
+        private string InfixToPrefix(string infix)
+        {
+            char[] tersInfix = infix.ToCharArray();
+            Array.Reverse(tersInfix);
+            string tersInfixStr = new string(tersInfix);
+
+            tersInfixStr = tersInfixStr.Replace('(', '#');
+            tersInfixStr = tersInfixStr.Replace(')', '(');
+            tersInfixStr = tersInfixStr.Replace('#', ')');
+
+            string postfix = InfixToPostfix(tersInfixStr);
+
+            string[] postfixDizi = postfix.Split(' ');
+            Array.Reverse(postfixDizi);
+            return string.Join(" ", postfixDizi);  
+        }
+        private void btnToPostfix_Click(object sender, EventArgs e)
+        {
+            string infix = txtInfix.Text;  
+            lblPostfix.Text = "Postfix: " + InfixToPostfix(infix);  
+        }
+        private void btnToPrefix_Click(object sender, EventArgs e)
+        {
+            string infix = txtInfix.Text;  
+            lblPrefix.Text = "Prefix: " + InfixToPrefix(infix);  
+        }
+    }
+}
